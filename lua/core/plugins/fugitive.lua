031a923b98e33337073d2514b7b9ea4396be82d0
dikesh <dikesh.jariwala@gmail.com>
2023-09-19 18:52:29 +0530

    fugitive keymap updated

--- lua/core/plugins/fugitive.lua
+++ lua/core/plugins/fugitive.lua
@@ -1,6 +1,6 @@
  return {
     "tpope/vim-fugitive",
      keys = {
-         { "<leader>gs", vim.cmd.Git, desc = "[G]it [S]tatus" }
+         { "<leader>gs", "<CMD>:vertical :Git<CR>", desc = "[G]it [S]tatus" }
      }
  }
