7dadc3d4ab4937916edc42562a5620900e966ef9
dikesh <dikesh.jariwala@gmail.com>
2023-09-20 14:17:13 +0530

    Delete buffer keymap added

--- lua/core/plugins/bufferline.lua
+++ lua/core/plugins/bufferline.lua
@@ -4,6 +4,7 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     keys = {
         {"<leader>n", "<CMD>:BufferLineCycleNext<CR>", mode = {"n"}, desc = "[N]ext Buffer" },
+        {"<leader>0", "<CMD>:bd<CR>", mode = {"n"}, desc = "Delete Buffer" },
         {"<leader>1", "<CMD>:BufferLineGoToBuffer 1<CR>", mode = {"n"}, desc = "Go To Buffer [1]" },
         {"<leader>2", "<CMD>:BufferLineGoToBuffer 2<CR>", mode = {"n"}, desc = "Go To Buffer [2]" },
         {"<leader>3", "<CMD>:BufferLineGoToBuffer 3<CR>", mode = {"n"}, desc = "Go To Buffer [3]" },
