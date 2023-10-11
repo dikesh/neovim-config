ed889a25e48a8f6994d270c8a17504a1329dd32c
dikesh <dikesh.jariwala@gmail.com>
2023-10-11 20:06:52 +0530

    Keymaps added and packages upgraded

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>bn", "<CMD>bnext<CR>", { desc = "[B]uffer [N]ext" })
 vim.keymap.set("n", "<leader>bd", "<CMD>bd<CR>", { desc = "[B]uffer [D]elete" })
+vim.keymap.set("n", "<leader>qq", vim.cmd.q, { desc = "[Q]uit" })
+vim.keymap.set("n", "<leader>ww", vim.cmd.w, { desc = "[W]rite" })
