a470dd987e325d5a08896a2ef03623584dc95cc9
dikesh <dikesh.jariwala@gmail.com>
2023-10-17 15:57:45 +0530

    Keymap updated and colorscheme bg made transparent

--- lua/core/plugins/catppuccin.lua
+++ lua/core/plugins/catppuccin.lua
@@ -5,6 +5,7 @@ return {
     config = function()
         require("catppuccin").setup({
             flavour = "macchiato",
+            transparent_background = true,
         })
 
         vim.cmd([[colorscheme catppuccin]])
--- lua/core/plugins/gitsigns.lua
+++ lua/core/plugins/gitsigns.lua
@@ -9,6 +9,7 @@ return {
         { "<leader>hn", "<CMD>Gitsigns next_hunk<CR>",       desc = "[H]unk [N]ext" },
         { "<leader>hs", "<CMD>Gitsigns stage_hunk<CR>",      desc = "[H]unk [S]tage" },
         { "<leader>hu", "<CMD>Gitsigns undo_stage_hunk<CR>", desc = "[H]unk [U]ndo Last Staged" },
+        { "<leader>hr", "<CMD>Gitsigns reset_hunk<CR>",      desc = "[H]unk [R]eset" },
         { "<leader>bl", "<CMD>Gitsigns blame_line<CR>",      desc = "[B]lame [L]ine" },
     }
 }
