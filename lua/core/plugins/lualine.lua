30644445caa3492ec102d775e0590818d3240c5c
dikesh <dikesh.jariwala@gmail.com>
2023-12-04 17:57:03 +0530

    Lualine config updated to show relative filepath

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -2,6 +2,9 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("lualine").setup()
+        -- Update config
+        local config = require('lualine').get_config()
+        config.sections.lualine_c = { { 'filename', path = 1 } }
+        require("lualine").setup(config)
     end
 }
