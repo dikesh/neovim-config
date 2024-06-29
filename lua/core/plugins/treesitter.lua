935f32acfe1ec99db60cefdf38264985b2341512
dikesh <dikesh.jariwala@gmail.com>
2024-06-29 19:46:04 +0530

    noice plugin added

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return {
                 "norg",
                 "python",
                 "query",
+                "regex",
                 "typescript",
                 "vim",
                 "vimdoc",
