1e1af777f2daaebde3fe889a5f199fa7c6fe67e2
dikesh <dikesh.jariwala@gmail.com>
2024-12-12 17:32:40 +0530

    flash.nvim plugin added

--- lua/core/plugins/neorg.lua
+++ lua/core/plugins/neorg.lua
@@ -3,36 +3,24 @@ return {
     dependencies = { "luarocks.nvim" },
     lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
-    config = function()
-        require('neorg').setup {
-            load = {
-                ["core.defaults"] = {},
-                ["core.concealer"] = {
-                    config = {
-                        folds = false,
-                        icons = {
-                            todo = {
-                                pending = {
-                                    icon = "",
-                                }
+    opts = {
+        load = {
+            ["core.defaults"] = {},
+            ["core.concealer"] = {
+                config = {
+                    folds = false,
+                    icons = {
+                        todo = {
+                            pending = {
+                                icon = "",
                             }
                         }
-                    },
-
-                },
-                ["core.dirman"] = {
-                    config = {
-                        workspaces = {
-                            home = "~/notes/home", -- Format: <name_of_workspace> = <path_to_workspace_root>
-                            work = "~/notes/work",
-                            arch = "~/notes/arch",
-                        },
-                        index = "index.norg", -- The name of the main (root) .norg file
                     }
                 },
-            }
+
+            },
         }
-    end,
+    },
     keys = {
         { "<leader>nh", "<CMD>Neorg workspace home<CR>", desc = "[N]eorg Workspace [H]ome" },
         { "<leader>nw", "<CMD>Neorg workspace work<CR>", desc = "[N]eorg Workspace [W]ork" },
--- lua/core/plugins/undotree.lua
+++
@@ -1,4 +0,0 @@
-return {
-    "mbbill/undotree",
-    keys = { { "<leader>ut", vim.cmd.UndotreeToggle, desc = "[U]ndotree [T]oggle" } }
-}
