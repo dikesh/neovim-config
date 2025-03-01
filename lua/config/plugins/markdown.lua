83b7afee89b342c61ba340b408ab383849435919
dikesh <dikesh.jariwala@gmail.com>
2025-03-01 17:44:34 +0530

    Packages updated

--- lua/config/plugins/markdown.lua
+++ lua/config/plugins/markdown.lua
@@ -1,4 +1,9 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        anti_conceal = {
+            enabled = false
+        }
+    }
 }
