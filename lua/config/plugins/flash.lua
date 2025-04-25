bc111017170574592222b86b04958dcdbf7b3b38
dikesh <dikesh.jariwala@gmail.com>
2025-04-25 11:40:27 +0530

    Python snippets added

--- lua/config/plugins/flash.lua
+++ lua/config/plugins/flash.lua
@@ -5,7 +5,7 @@ return {
     -- stylua: ignore
     keys = {
         {
-            "s",
+            "<c-f>",
             mode = { "n", "x", "o" },
             function() require("flash").jump() end,
             desc = "Flash"
