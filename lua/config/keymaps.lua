3a7afc6789ae8c666fdf07c98ce791bffb64dd23
dikesh <dikesh.jariwala@gmail.com>
2025-06-27 19:56:51 +0530

    Keymaps added to add / delete brackets / quotes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,19 @@ kmset("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "[M]ove [B]lock [D]own" })
 kmset("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "[M]ove [B]lock [U]p" })
 kmset("n", "<leader>sf", "<CMD>source %<CR>", { desc = "[S]ource [F]ile" })
 kmset("n", "<leader>ar", "<CMD>!hurl %<CR>", { desc = "Run Hurl file" })
+
+-- Add / Delete brackets / quotes around
+local kv = {
+    { '[', '[]' },
+    { '(', '()' },
+    { '{', '{}' },
+    { "'", "''" },
+    { '"', '""' },
+    { '`', '``' }
+}
+
+for _, v in pairs(kv) do
+    kmset("n", "<leader>a" .. v[1], "ciw" .. v[2] .. "<ESC>P", { desc = "Add " .. v[2] .. " around word" })
+    kmset("v", "<leader>a" .. v[1], "c" .. v[2] .. "<ESC>P", { desc = "Add " .. v[2] .. " around" })
+    kmset("n", "<leader>d" .. v[1], "di" .. v[1] .. "a<BS><BS><ESC>p", { desc = "Delete " .. v[2] .. " around" })
+end
