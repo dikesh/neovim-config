64bb46664b287f19720a0c3a0b449aa79abb4b40
dikesh <dikesh.jariwala@gmail.com>
2026-03-08 12:04:57 +0530

    Treesitter config updated according to main branch of the package

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,46 +1,56 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
     build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "c",
-                "comment",
-                "cpp",
-                "css",
-                "csv",
-                "go",
-                "hcl",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "nu",
-                "python",
-                "query",
-                "regex",
-                "scss",
-                "terraform",
-                "typescript",
-                "vala",
-                "vim",
-                "vimdoc",
-                "vue",
-                "yaml",
-            },
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-            },
+    init = function()
+        local parser_installed = {
+            "bash",
+            "c",
+            "comment",
+            "cpp",
+            "css",
+            "csv",
+            "go",
+            "hcl",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "nu",
+            "python",
+            "query",
+            "regex",
+            "scss",
+            "terraform",
+            "typescript",
+            "vala",
+            "vim",
+            "vimdoc",
+            "vue",
+            "yaml",
+        }
+
+        local ts = require("nvim-treesitter")
+
+        vim.defer_fn(function() ts.install(parser_installed) end, 1000)
+        ts.update()
+
+        -- auto-start highlights & indentation
+        vim.api.nvim_create_autocmd("FileType", {
+            desc = "User: enable treesitter highlighting",
+            callback = function(ctx)
+                -- highlights
+                local hasStarted = pcall(vim.treesitter.start) -- errors for filetypes with no parser
+
+                -- indent
+                local noIndent = {}
+                if hasStarted and not vim.list_contains(noIndent, ctx.match) then
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end,
         })
     end
 }
